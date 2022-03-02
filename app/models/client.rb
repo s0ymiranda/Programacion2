22005a569d38f20b629c7b5056325b34bfefa0ad
s0ymiranda <74148975+s0ymiranda@users.noreply.github.com>
2022-03-02 16:28:31 -0400

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,7 +5,7 @@ class Client < ApplicationRecord
   def self.company_name(company_name) 
     compani = Company.find_by_nombre(company_name)
     if !compani.nil?
-      Client.where(company_id: compani.id)
+      @clients = Client.where(company_id: compani.id)
     end
   end
 end
